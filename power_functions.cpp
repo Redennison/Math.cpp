9c5a070101fed7c181ad5d48bfabfb129032b068
Redennison <evan@dennisons.ca>
2021-09-24 11:32:32 -0400

    updated power_functions.cpp

--- power_functions.cpp
+++ power_functions.cpp
@@ -8,8 +8,7 @@ using namespace std;
 
 class Polynomial {
     public:
-        vector<pair<int, int>> points;
-        vector<vector<int>> equations;
+        vector<pair<int, int> > points;
         int V;
         int lc;
         int num_points; 
@@ -23,57 +22,7 @@ class Polynomial {
         void iso_vars();
         bool calc_degree();
         bool check_constant(int diff[], int num_diff);
-
 };
-
-void Polynomial::get_base_equations(int num_vars) {
-    int ls1, ls2;
-    vector<int> rs1(degree-1), rs2(degree-1);
-
-
-    // calculate all equations
-    // get 2 equations -> multiply equation 2 by first term equation 1 / first term equation 2
-    // subtract equation from previous equation
-    // call function with new equation
-    // return vector with values for variable
-    // if num_vars == 1 return vector with 1 value
-
-    // if num vars == 1 return it
-
-
-    // calculate first equations
-
-    for (int i=1;i<degree;i++) {
-        ls1 = points[i-1].second - lc * pow(points[i-1].first, degree);
-        for (int j=0,exp=degree-1;degree>=0;j++,exp--)
-            rs1[j] = pow(points[i-1].first, exp);
-        ls2 = points[i].second - lc * pow(points[i].first, degree);
-        for (int j=0,exp=degree-1;degree>=0;j++,exp--)
-            rs2[j] = pow(points[i].first, exp);
-        // call isolate function
-    }
-
-
-
-
-
-    // if (num_vars == 1) {
-    //     vector<int> values;
-    //     values.push_back(rs1[0]);
-    // }
-
-
-    // if num_vars == 1 do this then return
-}
-
-void Polynomial::iso_vars() {
-    // sub pts into eq (eliminate a)
-
-
-
-    // 3 vars left = 3 equations
-    // recursion
-}
 
 void Polynomial::get_lc() {
     int n = 1;
@@ -109,9 +58,9 @@ bool Polynomial::check_constant(int diff[], int num_diff) {
 // Calculates degree of polynomial 
 // Returns true if successfull/false if unsuccessful
 bool Polynomial::calc_degree() {
-    int diff[num_points], num_diff = num_points;
+    int diff[num_points], num_diff = num_points; 
     for (int i=0;i<num_points;i++)
-        diff[i] = points[i].second;
+        diff[i] = points[i].second; 
 
     // if all y's the same at start then line is horizontal
     while ((!check_constant(diff, num_diff)) and (num_diff > 1)) {
@@ -133,10 +82,20 @@ bool Polynomial::calc_degree() {
 void Polynomial::get_function() {
     get_points();
     if (calc_degree()) {
-        equations.resize(degree);
         get_lc();
-        // call get as degree+1
-        cout << "y = " << lc << "x^" << degree;
+        if (degree == 1) {
+            if (lc == 1)
+                cout << "f(x) = " << "x" << "\n";     
+            else 
+                cout << "f(x) = " << lc << "x" << "\n";
+        } else if (degree == 0) 
+            cout << "f(x) = " << lc << "\n"; 
+        else {
+            if (lc == 1)
+                cout << "f(x) = " << "x^" << degree << "\n"; 
+            else 
+                cout << "f(x) = " << lc << "x^" << degree << "\n";               
+        }
     } 
 }
 
